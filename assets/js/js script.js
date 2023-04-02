01454f70af73e5f4b41dccffe2b444ba68d10bef
Orlando Nelson <nelson8223@gmail.com>
2023-04-02 15:46:43 -0400

    updated index.html

--- assets/js/js script.js
+++ assets/js/js script.js
@@ -1,183 +1,133 @@
-// var timerEl = document.getElementById('countdown');
-// const nextButton = document.getElementById('next-btn')
-// var score = document.getElementById ("score")
-// const questionElement = document.getElementById('question')
-// const answerButtonsElement= document.getElementById('answer-buttons')
-// const questionContainerElement = document.getElementById('question-container')
-// const startButton = document.getElementById('start-btn')
-// var finalScore= document.querySelector("first place")
-// var finalScore= document.querySelector("second place")
-// var finalScore= document.querySelector("third place")
+// Use const instead of var for variables that should not be reassigned
+const timerEl = document.getElementById('countdown');
+const nextButton = document.getElementById('next-btn');
+const score = document.getElementById("score");
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const questionContainerElement = document.getElementById('question-container');
+const startButton = document.getElementById('start-btn');
+const finalScoreFirst = document.querySelector("#firstplace"); // Use # for ID selectors
+const finalScoreSecond = document.querySelector("#secondplace");
+const finalScoreThird = document.querySelector("#thirdplace");
 
-var questionBox= document.querySelector("#qs")
-var currentIndex= 0
+const questionBox = document.querySelector("#qs");
+let currentIndex = 0;
 
-var questions =[
+const questions = [
     { 
-        questionText: " question 1 text",
-        options: ["answer1","answer2","answer3","answer4",],
+        questionText: "question 1 text",
+        options: ["answer1", "answer2", "answer3", "answer4"],
         correct: "answer2"
     },
     { 
-        questionText: " question 2 text",
-        options: ["answer1","answer2","answer3","answer4",],
+        questionText: "question 2 text",
+        options: ["answer1", "answer2", "answer3", "answer4"],
         correct: "answer2"
     },
     { 
-        questionText: " question 3 text",
-        options: ["answer1","answer2","answer3","answer4",],
+        questionText: "question 3 text",
+        options: ["answer1", "answer2", "answer3", "answer4"],
         correct: "answer2"
-    },
+    }
+];
 
-]
-  
+console.log(questions[1].questionText);
 
-console.log(questions[1].questionText)
+function getQuestion() {
+    const thisQ = questions[currentIndex];
+    console.log(thisQ, "thisQ");
+    questionBox.textContent = thisQ.questionText;
 
-function getQuestion(){
-    var thisQ= questions[currentIndex]
-    console.log(thisQ,"thisQ")
-questionBox.textContent= questions[1].questionText
-
-for (let i = 0; i < thisQ.options.length; i++) {
-    var answer =thisQ.options[i]
-    console.log(answer)
-
-    var button =document.createElement("button")
-    button.textContent=answer
-    var bodyEl= document.querySelector("body")
-    bodyEl.appendChild(button)
-
+    for (let i = 0; i < thisQ.options.length; i++) {
+        const answer = thisQ.options[i];
+        console.log(answer);
+        const button = document.createElement("button");
+        button.textContent = answer;
+        answerButtonsElement.appendChild(button);
+    }
 }
 
+getQuestion();
 
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+    console.log("started");
+    setNextQuestion();
 }
-getQuestion()
-// startButton.addEventListener('click',startGame)
-// function startGame() {
-// console.log("started")
-// setNextQuestion()
-// }
 
-// nextButton.addEventListener('click,', () => {
-//     currentQuestion++,
-//     setNextQuestion()
-//   } )
+nextButton.addEventListener('click', () => {
+    currentIndex++;
+    setNextQuestion();
+});
 
-// function setNextQuestion(){
-//     resetState()
-// showQuestion(listQuestion[currentQuestion])
+function setNextQuestion() {
+    resetState();
+    showQuestion(questions[currentIndex]);
+} 
 
-// } 
-
-// function showQuestion(question){
-// questionElement.innerText= question.question
-// question.selectAnswer.forEach(element => {
-//     const buttons = document.createElement('button')
-//     button.innerText= answer.text 
-//     buttons.classList.add('btn')
-//     if (answer.correct) {
-//         button.dataset.correct = answer.correct
-
-//     }
-
-//     buttons.addEventListener('click', selectAnswer)
-//     answerButtonsElement.appendChild(buttons)
+function showQuestion(question) {
+    questionElement.innerText = question.questionText;
+    answerButtonsElement.innerHTML = ""; // Clear previous buttons
     
-// });
+    question.options.forEach(option => {
+        const button = document.createElement('button');
+        button.innerText = option;
+        button.classList.add('btn');
+        
+        if (option === question.correct) {
+            button.dataset.correct = true;
+        }
+        
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
+}
 
-// }
-
-
-// function selectAnswer(i){
-//     const selectedButton= i.target
-//     const correct = selectedButton.dataset.correct
-//     setStatusClass(document.body, correct)
-//     Array.from(answerButtonsElement.children).forEach(button=>{
-//         setStatusClass(button,button.dataset.correct)
-//     })
-// function setStatusClass( element,correct){
-// clearStatusClass(element)
-// if (correct){
-//     element.classList.add('correct')
-// } else{
-//     element.classList.add('wrong')
-// }
-
-// }
-
-
-// }
-
-// saveButton.addEventListener("click", function(event) {
-//     event.preventDefault();
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
     
-// var score = {
-// score: score.value,
-// };
-// localStorage.setItem("Final Score", JSON.stringify(score));
-// renderMessage();
+    setStatusClass(document.body, correct);
+    
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+}
 
-// })
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+}
 
-// const question01 = [ 
-// {
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
 
-//     question: "Javascript is type of language?",
-//     answer: [ 
-//           { text: "Object-oriented", correct: true},
-//           { text: "Spanish", wrong: false },
-//           { text: "Procedural", wrong: false }
+const saveButton = document.getElementById("save-btn");
 
-//     ]
-// }
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    const user = {
+        firstPlace: finalScoreFirst.value.trim(),
+        SecondPlace: finalScoreSecond.value.trim(),
+        thirdPlace: finalScoreThird.value.trim()
+    };
+    
+    localStorage.setItem("Final Score", JSON.stringify(user));
+    renderMessage();
+});
 
-// ]
+const question01 = [ 
+    {
+        question: "Javascript is type of language?",
+        answer: []
 
-// // const question02 = [
-// // {
-// //   question: "How can a datatype be declared to be a constant type?",
-// //   answer: [
-// //       {text: "var", wrong: false },
-// //       {text: "const", correct: true };
-// //       text: "let", wrong: false }
-
-// //   ]
-
-// // }
-
-// const question03 = " How to stop an interval timer in Javascript?"
-// answer: [ 
-//     {text: "intervalOver", wrong: false},
-//     { text: " clearInterval", correct: true},
-//     { text: "cleartimer", wrong: false }
-
-
-// ]
-// var user = {
-//     firstPlace: firstplace.value.trim(),
-//     SecondPlace: SecondPlace.value.trim(),
-//     thirdPlace: thirdPlace.value.trim(),
-// }
-
-
-
-
-// function countdown() {
-// var timeLeft =120;
-
-// var timeInterval= setInterval(function() {
-
-//     timerEl.textContent = timeLeft + "seconds remaining";
-//     timeLeft--;
-// })
-//     if(timeLeft===0) {
-//    clearInterval(timeInterval);
-//    sendMessage();
-// }
-// function sendMessage() {
-//     timeEl.textContent = "Game Over ";
-
-// }
-// setTime();
-// }
+    }]
